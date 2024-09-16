2690c15f8e3b38ac6a7ff8be8f3def10d8fe556e
pablo-gomez10 <118767574+pablo-gomez10@users.noreply.github.com>
2024-09-16 06:37:55 -0300

    Update whatsapp.js

--- whatsapp.js
+++ whatsapp.js
@@ -7,7 +7,7 @@
     // Este es evento de clic al botón
     whatsappButton.addEventListener("click", function() {
       // Obtener el número de teléfono y el texto del mensaje
-      var phoneNumber = "5493571676981"; // Reemplaza con el número de teléfono deseado
+      var phoneNumber = "5491150575131"; // Reemplaza con el número de teléfono deseado
       var messageText = "Hola, ¿cómo estás?"; // Reemplaza con el texto del mensaje deseado
   
       // Crear el enlace de WhatsApp con el número de teléfono y el texto del mensaje codificados
